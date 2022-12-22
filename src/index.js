80dfd8cfdded393e810b85f493b8c2bb9eb06254
Rustem Dzhuraiev <73857551+RustDzhur@users.noreply.github.com>
2022-12-22 02:33:56 +0200

    fix bugs

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="goit-react-hw-08-phonebook">
+        <BrowserRouter basename="contact-book-ReactJS">
           <App />
           <Toaster position="top-center" reverseOrder={false} />
         </BrowserRouter>
